5b25d76a9cfc5f8d06f473249ea724092def56c2
Allison Inman <t-alinma@microsoft.com>
2018-07-24 15:11:47 -0700

    remove unused member variables

--- src/app/shapes/Rectangle.ts
+++ src/app/shapes/Rectangle.ts
@@ -2,15 +2,10 @@ import * as Konva from "konva";
 import {Shape} from "./Shape";
 
 export class Rectangle extends Shape {
-  private width: number;
-  private height: number;
-
   private strokeWidth: number = 2;
 
   public constructor(name: string, width: number, height: number){
     super(name);
-    this.width = width;
-    this.height = height;
     this.instance = this.createKonvaRectangle(width, height);
   }
   
