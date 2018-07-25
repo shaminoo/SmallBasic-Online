e5503a8d66e0bab5680b092b25c9e45c40038177
Allison Inman <t-alinma@microsoft.com>
2018-07-25 11:23:25 -0700

    add animate function to shape

--- src/app/shapes/Shape.ts
+++ src/app/shapes/Shape.ts
@@ -7,6 +7,7 @@ export abstract class Shape {
     // abstract methods
     public abstract getLeft(): number;
     public abstract getTop(): number;
+    public abstract move(x: number, y: number): void;
 
     public constructor(name: string){
         this.name = name;
@@ -23,16 +24,35 @@ export abstract class Shape {
     public showShape(): void {
         this.instance.show();
     }
+
+    public zoom(scaleX: number, scaleY: number): void {
+        this.instance.scale({x: scaleX, y: scaleY});
+    }
     
     public remove(): void {
-        // TODO
+        this.instance.destroy();
     }
     
-    public move(): void {
-        // TODO
+    public rotate(degrees: number): void {
+        this.instance.rotation(degrees);
     }
-    
-    public rotate(): void {
-        // TODO
+
+    public animate(x: number, y: number, durationInMilliseconds: number, layer: Konva.Layer): void{
+        const self: Shape = this;
+        const startX: number = this.getLeft();
+        const startY: number = this.getTop();
+        const xDistanceToMove: number = x - this.getLeft();
+        const yDistanceToMove: number = y - this.getTop();
+        const animation: Konva.Animation = new Konva.Animation((frame) =>{
+            const percentage: number = frame.time /durationInMilliseconds;
+            const newX: number = startX + xDistanceToMove * percentage;
+            const newY: number = startY + yDistanceToMove * percentage;
+            self.move(newX, newY);
+            if(frame.time >= durationInMilliseconds){
+                animation.stop();
+                self.move(x, y);
+            }
+        }, layer);
+        animation.start();
     }
 }
